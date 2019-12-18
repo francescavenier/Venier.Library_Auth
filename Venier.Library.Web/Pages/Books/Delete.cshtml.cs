5e7e371c6e32c733ab8a2920283ed589b8008227
francescavenier <francescavenier10@gmail.com>
2019-12-18 12:48:11 +0100

    Add option to delete books

--- Venier.Library.Web/Pages/Books/Delete.cshtml.cs
+++ Venier.Library.Web/Pages/Books/Delete.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Venier.Library.Data;
+using Venier.Library.Data.Models;
 
 namespace Venier.Library.Web
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet(int id)
-        {
+        private readonly IBooksRepository _booksRepository;
 
+
+        public DeleteModel(IBooksRepository booksRepository, IBookReadHistoriesRepository bookReadHistoriesRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+        public Book Book { get; set; }
+        public PageResult OnGet(int id)
+        {
+            _booksRepository.Delete(id);
+            return Page();
         }
     }
 }
