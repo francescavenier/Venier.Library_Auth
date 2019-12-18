d59789a49296571a6818d97bf4d1717528723d32
francescavenier <francescavenier10@gmail.com>
2019-12-18 11:48:39 +0100

    Add Services in Api

---
+++ Venier.Library.Api/Services/BooksResponseDataServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Venier.Library.Api.Services
+{
+    class BooksResponseDataServices
+    {
+        public async Task<IEnumerable<Doc>> GetResponse(string text)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://openlibrary.org/search.json?q=" + text);
+            response.EnsureSuccessStatusCode();
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            var obj = await JsonSerializer.DeserializeAsync<BooksResponse>(stream);
+            return obj.docs;
+        }
+    }
+}
---
+++ Venier.Library.Api/Services/IBooksResponseDataServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Venier.Library.Api.Services
+{
+    interface IBooksResponseDataServices
+    {
+        Task<IEnumerable<Doc>> GetResponse(string text);
+    }
+}
