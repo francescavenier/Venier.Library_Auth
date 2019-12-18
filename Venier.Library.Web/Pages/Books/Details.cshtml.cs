4042f214cacb395031e5d151618c1f262cca691e
francescavenier <francescavenier10@gmail.com>
2019-12-18 12:07:57 +0100

    Add Details page

---
+++ Venier.Library.Web/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Venier.Library.Data;
+using Venier.Library.Data.Models;
+
+namespace Venier.Library.Web.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IBooksRepository _booksRepository;
+        private readonly IBookReadHistoriesRepository _bookReadHistoriesRepository;
+
+
+        public DetailsModel(IBooksRepository booksRepository, IBookReadHistoriesRepository bookReadHistoriesRepository)
+        {
+            _booksRepository = booksRepository;
+            _bookReadHistoriesRepository = bookReadHistoriesRepository;
+        }
+        public Book Book { get; set; }
+        public IEnumerable<BookReadHistory> History { get; set; }
+        public void OnGet(int id)
+        {
+            Book = _booksRepository.Get(id);
+            History = _bookReadHistoriesRepository.GetByBook(id);
+        }
+    }
+}
