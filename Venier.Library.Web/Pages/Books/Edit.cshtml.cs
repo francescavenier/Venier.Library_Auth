75af4174fc4e6b4e5b3ad79cecc8f1eaf36abf12
francescavenier <francescavenier10@gmail.com>
2019-12-23 12:46:04 +0100

    OnPost in Edit

--- Venier.Library.Web/Pages/Books/Edit.cshtml.cs
+++ Venier.Library.Web/Pages/Books/Edit.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Venier.Library.Data;
+using Venier.Library.Data.Models;
 
 namespace Venier.Library.Web
 {
     public class EditModel : PageModel
     {
+        private readonly IBooksRepository _booksRepository;
+        private readonly IBookReadHistoriesRepository _bookReadHistoriesRepository;
+        
+        public EditModel(IBooksRepository booksRepository, IBookReadHistoriesRepository bookReadHistoriesRepository)
+        {
+            _booksRepository = booksRepository;
+            _bookReadHistoriesRepository = bookReadHistoriesRepository;
+        }
+        
+        [BindProperty]
+        public Book Book { get; set; }
+        public IEnumerable<BookReadHistory> History { get; set; }
+
         public void OnGet(int id)
         {
+            Book = _booksRepository.Get(id);
+        }
+
+        public IActionResult OnPost(int id)
+        {
+                _booksRepository.Update(Book);
+                return RedirectToPage("/Index"); // da modificare il redirect
 
         }
     }
