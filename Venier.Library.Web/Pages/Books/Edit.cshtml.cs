be1fe03743bf746e48097a7ff6f085a52ae63e67
francescavenier <francescavenier10@gmail.com>
2020-01-07 15:24:16 +0100

    edit single book

--- Venier.Library.Web/Pages/Books/Edit.cshtml.cs
+++ Venier.Library.Web/Pages/Books/Edit.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,41 +20,74 @@ namespace Venier.Library.Web
             _booksRepository = booksRepository;
             _bookReadHistoriesRepository = bookReadHistoriesRepository;
         }
-
-
+        
         [BindProperty]
+        public EditBook editBook { get; set; }
         public Book Book { get; set; }
+        
         public IEnumerable<BookReadHistory> History { get; set; }
 
-        string Title { get; set; }
-        string Author { get; set; }
-        string Description { get; set; }
-        DateTime CreationDate { get; set; }
-        byte? GenreId { get; set; }
-        DateTime? PublishedDate { get; set; }
-        bool IsRead { get; set; }
-        DateTime? DateLastUpdate { get; set; }
-                     
+        public class EditBook
+        {
+            [Display(Name = "Title")]
+            [Required]
+            public string Title { get; set; }
+
+            [Display(Name = "Author")]
+            public string Author { get; set; }
+
+            [Display(Name = "Description")]
+            public string Description { get; set; }
+
+            [Display(Name = "Creation Date")]
+            public DateTime CreationDate { get; set; }
+
+            [Display(Name = "Genre")]
+            public byte? GenreId { get; set; }
+
+            [Display(Name = "Published Date")]
+            public DateTime? PublishedDate { get; set; }
+
+            [Display(Name = "Is read?")]
+            public bool IsRead { get; set; }
+
+            [Display(Name = "Date of last update")]
+            public DateTime? DateLastUpdate { get; set; }
+        }
+
         public void OnGet(int id)
         {
             Book = _booksRepository.Get(id);
-            Title = Book.Title;
-            Author = Book.Author;
-            Description = Book.Description;
-            CreationDate = Book.CreationDate;
-            GenreId = Book.GenreId;
-            PublishedDate = Book.PublishedDate;
-            IsRead = Book.IsRead;
-            DateLastUpdate = Book.DateLastUpdate;
+            this.editBook = new EditBook {
+                Author = Book.Author,
+                Title = Book.Title,
+                Description = Book.Description,
+                CreationDate = Book.CreationDate,
+                GenreId = Book.GenreId,
+                PublishedDate = Book.PublishedDate,
+                IsRead = Book.IsRead,
+                DateLastUpdate = Book.DateLastUpdate
+            };
         }
 
         public IActionResult OnPost(int id)
         {
-            Book = new Book { Author = Author, DateLastUpdate = DateTime.Now}; 
-            //Book.DateLastUpdate = DateTime.Now;
-            _booksRepository.Update(Book);
-            return RedirectToPage("/Index"); // da modificare il redirect
-
+            if (ModelState.IsValid)
+            {
+                Book = new Book { 
+                    Author = editBook.Author,
+                    Title = editBook.Title,
+                    Description = editBook.Description,
+                    CreationDate = editBook.CreationDate,
+                    GenreId = editBook.GenreId,
+                    PublishedDate = editBook.PublishedDate,
+                    IsRead = editBook.IsRead,
+                    DateLastUpdate = DateTime.Now };
+                //Book.DateLastUpdate = DateTime.Now;
+                _booksRepository.Update(Book);
+                return RedirectToPage("/Index"); // da modificare il redirect
+            }
+            return Page();
         }
     }
 }
